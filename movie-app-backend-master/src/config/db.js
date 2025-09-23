e218835d10749fc58f911278c81903cd0e2d6e66
AbhishekJadhav1996 <abhishekjad@cybage.com>
2025-09-24 01:02:10 +0530

    db.js added

--- movie-app-backend-master/src/config/db.js
+++ movie-app-backend-master/src/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: process.env.MONGO_DB_NAME || undefined,
     });
     console.log('✅ MongoDB connected successfully');
   } catch (error) {
